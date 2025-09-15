39151d3db6894f160879ec86ec7b80f860a182fe
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-09-15 17:57:09 +0530

    the reverse program added

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 
-let arr=[5,7,3,8,4,10];
-n=arr.length;
-for(i=0;i<n;i++)
-{
-    for(j=0;j<n-i;j++)
-    {
-        if(arr[j]>arr[j+1])
-        {
-            temp=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp;
+// let arr=[5,7,3,8,4,10];
+// n=arr.length;
+// for(i=0;i<n;i++)
+// {
+//     for(j=0;j<n-i;j++)
+//     {
+//         if(arr[j]>arr[j+1])
+//         {
+//             temp=arr[j];
+//             arr[j]=arr[j+1];
+//             arr[j+1]=temp;
            
-        }
+//         }
 
-    }
+//     }
 
    
+// }
+
+//  console.log(arr[n-2]);
+
+
+
+let arr = [1, 2, 3, 4, 5]; 
+let reversed = [];           
+
+for (let i = arr.length - 1; i >= 0; i--) {
+  reversed.push(arr[i]);    
 }
 
- console.log(arr[n-2]);
+console.log("Original:", arr);
+console.log("Reversed:", reversed);
+
